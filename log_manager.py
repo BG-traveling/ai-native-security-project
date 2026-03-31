71078fe4b9de996fe45728c56680a172156df709
김동현 <hades010101@naver.com>
2026-03-31 17:11:30 +0900

    로그 생성 결과를 리스트에 저장하는 구조 추가

--- log_manager.py
+++ log_manager.py
@@ -13,14 +13,28 @@ def create_login_log(user_id, ip, target, status):
     
     return login_log
 
+
 # create_login_log 함수 동작 테스트
+multi_log_list = []
+
 login_log = create_login_log(
     user_id="admin",
     ip="192.168.0.10",
     target="DB_SERVER_1",
     status="SUCCESS"
 )
+multi_log_list.append(login_log)
 
-print(login_log)
+login_log = create_login_log(
+    user_id="user",
+    ip="192.168.1.10",
+    target="DB_SERVER_1",
+    status="SUCCESS"
+)
+ 
+multi_log_list.append(login_log)
+
+# print(login_log)
+print(multi_log_list)
     
     
