3f87ef10c584378295d92396530326f0af548171
김동현 <hades010101@naver.com>
2026-04-01 10:37:00 +0900

    1분 내 4회 이상 로그인 시도 감지 로직 구현

--- log_manager.py
+++ log_manager.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def create_login_log(user_id, ip, target, status):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -23,15 +23,39 @@ def add_login_log(log_list, user_id, ip, target, status):
 
 add_login_log(log_list, "admin", "192.168.10.1", "DB_SERVER_1", "Success")
 add_login_log(log_list, "admin2", "192.168.10.2", "DB_SERVER_1", "Success")
+add_login_log(log_list, "admin2", "192.168.10.2", "DB_SERVER_1", "Success")
+add_login_log(log_list, "admin2", "192.168.10.2", "DB_SERVER_1", "Success")
+add_login_log(log_list, "admin2", "192.168.10.2", "DB_SERVER_1", "Success")
+
+
 
 # 로그 출력 확인 테스트용 함수
-# def log_output(log_list):
-#     for i, log in enumerate(log_list, start=1):
-#         print(f"[{i}번 로그]")
-#         print(f"사용자 ID: {log['user_id']}")
-#         print(f"IP       : {log['ip']}")
-#         print(f"대상     : {log['target']}")
-#         print(f"결과     : {log['status']}")
-#         print("-" * 30)
+def log_output(log_list):
+    for i, log in enumerate(log_list, start=1):
+        print(f"[{i}번 로그]")
+        print(f"사용자 ID: {log['user_id']}")
+        print(f"IP       : {log['ip']}")
+        print(f"대상     : {log['target']}")
+        print(f"결과     : {log['status']}")
+        print("-" * 30)
         
-# log_output(log_list)
+log_output(log_list)
+
+def detect_excessive_login(log_list, user_id):
+    now = datetime.now()
+    
+    count = 0
+    
+    for log in log_list:
+        if log['user_id'] == user_id:
+            log_time = datetime.strptime(log["time"], "%Y-%m-%d %H:%M:%S")
+            
+            if now - log_time <= timedelta(minutes=1):
+                count += 1
+    if count >= 4:
+        return True
+    else:
+        return False
+    
+result = detect_excessive_login(log_list, user_id="admin2")
+print(result)   
