1e4da79e9d70fe74f0bdaafa2ec01af24befe658
김동현 <hades010101@naver.com>
2026-04-01 11:26:36 +0900

    IP 기반 접근 감지 로직 추가 및 로그 추가 함수에 연결

--- log_manager.py
+++ log_manager.py
@@ -1,7 +1,12 @@
 from datetime import datetime, timedelta
 
-# create_login_log 함수 동작 테스트
+
 log_list = []
+
+allowed_ips = [
+    "192.168.10.1",
+    "192.168.10.2"
+]
 
 def create_login_log(user_id, ip, target, status):
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -31,10 +36,18 @@ def detect_excessive_login(log_list, user_id):
     else:
         return False
 
+def detect_unallowed_ip(ip, allowed_ips):
+    return ip not in allowed_ips
+    
 def add_login_log(log_list, user_id, ip, target, status):
     log_list.append(create_login_log(user_id, ip, target, status))
     if detect_excessive_login(log_list, user_id):
         print("과도한 로그인 요청 감지")
+        
+    if detect_unallowed_ip(ip, allowed_ips):
+        print("허용되지 않은 IP 접근 감지")
+        
+    
 # 로그 출력 확인 테스트용 함수
 def log_output(log_list):
     for i, log in enumerate(log_list, start=1):
@@ -51,8 +64,9 @@ add_login_log(log_list, "admin2", "192.168.10.2", "DB_SERVER_1", "Success")
 add_login_log(log_list, "admin2", "192.168.10.2", "DB_SERVER_1", "Success")
 add_login_log(log_list, "admin2", "192.168.10.2", "DB_SERVER_1", "Success")
 add_login_log(log_list, "admin2", "192.168.10.2", "DB_SERVER_1", "Success")
+add_login_log(log_list, "admin3", "192.168.10.4", "DB_SERVER_1", "Success")
 
-log_output(log_list)
+# log_output(log_list)
     
 # result = detect_excessive_login(log_list, user_id="admin2")
 # print(result)   
