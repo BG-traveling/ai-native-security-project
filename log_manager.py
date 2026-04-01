9b948a7542f94f3414d8a6fcaafe24f1fed5eaf3
김동현 <hades010101@naver.com>
2026-04-01 10:04:16 +0900

    add_login_log 함수 추가 및 로그 누적 구조 구현

--- log_manager.py
+++ log_manager.py
@@ -12,29 +12,26 @@ def create_login_log(user_id, ip, target, status):
     }
     
     return login_log
-
+    
 
 # create_login_log 함수 동작 테스트
-multi_log_list = []
+log_list = []
 
-login_log = create_login_log(
-    user_id="admin",
-    ip="192.168.0.10",
-    target="DB_SERVER_1",
-    status="SUCCESS"
-)
-multi_log_list.append(login_log)
+def add_login_log(log_list, user_id, ip, target, status):
+    log_list.append(create_login_log(user_id, ip, target, status))
+    return 
 
-login_log = create_login_log(
-    user_id="user",
-    ip="192.168.1.10",
-    target="DB_SERVER_1",
-    status="SUCCESS"
-)
- 
-multi_log_list.append(login_log)
+add_login_log(log_list, "admin", "192.168.10.1", "DB_SERVER_1", "Success")
+add_login_log(log_list, "admin2", "192.168.10.2", "DB_SERVER_1", "Success")
 
-# print(login_log)
-print(multi_log_list)
-    
-    
+# 로그 출력 확인 테스트용 함수
+# def log_output(log_list):
+#     for i, log in enumerate(log_list, start=1):
+#         print(f"[{i}번 로그]")
+#         print(f"사용자 ID: {log['user_id']}")
+#         print(f"IP       : {log['ip']}")
+#         print(f"대상     : {log['target']}")
+#         print(f"결과     : {log['status']}")
+#         print("-" * 30)
+        
+# log_output(log_list)
